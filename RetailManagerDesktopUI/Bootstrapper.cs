8869438fb7556f110d8fc4b0e16e721f36ef2af3
Denis <kozyrevd2001@ukr.net>
2021-09-09 16:48:46 +0300

    Add wpf+mvvm UI, configured wpf UI with Caliburn Micro lib, Adds DI for viewModels

---
+++ RetailManagerDesktopUI/Bootstrapper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+using RetailManagerDesktopUI.ViewModels;
+
+namespace RetailManagerDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private SimpleContainer simpleContainer = new SimpleContainer();
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+        protected override void Configure()
+        {
+            simpleContainer.Instance(simpleContainer);
+            simpleContainer
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel")).ToList()
+                .ForEach(type => simpleContainer.RegisterPerRequest(type,type.ToString(),type));
+
+        }
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+        protected override object GetInstance(Type service, string key)
+        {
+            return simpleContainer.GetInstance(service, key);
+        }
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return simpleContainer.GetAllInstances(service);
+        }
+        protected override void BuildUp(object instance)
+        {
+            simpleContainer.BuildUp(instance);
+        }
+    }
+}
